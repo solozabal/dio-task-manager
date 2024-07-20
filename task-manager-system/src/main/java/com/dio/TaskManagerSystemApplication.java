e0d37b4ce1228151cdf0fbcbf8d732d625b2c88f
Pedro Solozabal <pedrosolozabal@gmail.com>
2024-07-20 18:11:19 -0300

    Updates pom.xml. Create application.properties file. Trying to put SwaggerConfig class.

---
+++ task-manager-system/src/main/java/com/dio/TaskManagerSystemApplication.java
@@ -0,0 +1,12 @@
+package com.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSystemApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerSystemApplication.class, args);
+    }
+}
--- task-manager-system/src/main/java/com/dio/facade/TaskFacade.java
+++ task-manager-system/src/main/java/com/dio/facade/TaskFacade.java
@@ -1,11 +1,15 @@
 package com.dio.facade;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.dio.model.Task;
 import com.dio.repository.TaskRepository;
 import com.dio.strategy.TaskPrioritizationStrategy;
 
+@Service
 public class TaskFacade {
     @Autowired
     private TaskRepository taskRepository;
